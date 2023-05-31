183f7e13ca080871806ac3fa109d151593f5a68a
haphut <haphut@mistmap.com>
2023-05-31 22:48:29 +0300

    test: Add tests for simple mechanism

---
+++ tests/mechanisms/simple/test_hyperparameter_optimization.py
@@ -0,0 +1,36 @@
+import numpy as np
+from apc_anonymizer.mechanisms.simple import hyperparameter_optimization
+
+
+def test_get_category_bounds():
+    vehicle_model = {
+        "outputFilenames": ["foo.csv", "bar.csv"],
+        "minimumCounts": {
+            "EMPTY": 0,
+            "MANY_SEATS_AVAILABLE": 6,
+            "FEW_SEATS_AVAILABLE": 36,
+            "STANDING_ROOM_ONLY": 46,
+            "CRUSHED_STANDING_ROOM_ONLY": 84,
+            "FULL": 110,
+        },
+        "maximumCount": 126,
+    }
+    expected = [(0, 5), (6, 35), (36, 45), (46, 83), (84, 109), (110, 126)]
+    assert (
+        hyperparameter_optimization.get_category_bounds(vehicle_model)
+        == expected
+    )
+
+
+def test_normalize_probabilities():
+    probs = np.array(
+        [
+            [0.1234, 0.2345, 0.3456],
+            [0.4567, 0.5678, 0.6789],
+            [0.7890, 0.8901, 0.9012],
+            [0.9123, 0.9234, 0.9345],
+            [0.9456, 0.9567, 0.9678],
+        ]
+    )
+    normalized = hyperparameter_optimization.normalize_probabilities(probs)
+    assert np.all(np.abs(normalized.sum(axis=1) - 1.0) < 1e-10)
---
+++ tests/mechanisms/simple/test_initial.py
@@ -0,0 +1,76 @@
+import pandas as pd
+from apc_anonymizer.mechanisms.simple import initial
+
+
+def test_simplest_initial_dataframe():
+    vehicleModel = {
+        "outputFilename": "foo",
+        "minimumCounts": {
+            "EMPTY": 0,
+            "FULL": 1,
+        },
+        "maximumCount": 1,
+    }
+    expected_initial_df = pd.DataFrame(
+        data={"EMPTY": [1.0, 0.0], "FULL": [0.0, 1.0]}
+    )
+    initial_df = initial.create_initial_dataframe(vehicleModel)
+    assert initial_df.equals(expected_initial_df)
+
+
+def test_simple_initial_dataframe():
+    vehicleModel = {
+        "outputFilename": "foo",
+        "minimumCounts": {
+            "EMPTY": 0,
+            "STANDING_ROOM_ONLY": 1,
+            "FULL": 2,
+        },
+        "maximumCount": 3,
+    }
+    expected_initial_df = pd.DataFrame(
+        data={
+            "EMPTY": [1.0, 0.0, 0.0, 0.0],
+            "STANDING_ROOM_ONLY": [0.0, 1.0, 0.0, 0.0],
+            "FULL": [0.0, 0.0, 1.0, 1.0],
+        }
+    )
+    initial_df = initial.create_initial_dataframe(vehicleModel)
+    assert initial_df.equals(expected_initial_df)
+
+
+def test_realistic_initial_dataframe():
+    vehicleModel = {
+        "outputFilename": "volvo-8908rle.csv",
+        "minimumCounts": {
+            "EMPTY": 0,
+            "MANY_SEATS_AVAILABLE": 6,
+            "FEW_SEATS_AVAILABLE": 36,
+            "STANDING_ROOM_ONLY": 46,
+            "CRUSHED_STANDING_ROOM_ONLY": 84,
+            "FULL": 110,
+        },
+        "maximumCount": 126,
+    }
+    expected_initial_df = pd.DataFrame(
+        data={
+            "EMPTY": ([1.0] * (6 - 0)) + ([0.0] * (126 - 6 + 1)),
+            "MANY_SEATS_AVAILABLE": ([0.0] * 6)
+            + ([1.0] * (36 - 6))
+            + ([0.0] * (126 - 36 + 1)),
+            "FEW_SEATS_AVAILABLE": ([0.0] * 36)
+            + ([1.0] * (46 - 36))
+            + ([0.0] * (126 - 46 + 1)),
+            "STANDING_ROOM_ONLY": ([0.0] * 46)
+            + ([1.0] * (84 - 46))
+            + ([0.0] * (126 - 84 + 1)),
+            "CRUSHED_STANDING_ROOM_ONLY": ([0.0] * 84)
+            + ([1.0] * (110 - 84))
+            + ([0.0] * (126 - 110 + 1)),
+            "FULL": ([0.0] * 110)
+            + ([1.0] * (126 - 110))
+            + ([1.0] * (126 - 126 + 1)),
+        }
+    )
+    initial_df = initial.create_initial_dataframe(vehicleModel)
+    assert initial_df.equals(expected_initial_df)
